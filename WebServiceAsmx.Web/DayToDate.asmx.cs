493857797f93424e280e3f0c5f4560a0dfeddef0
Wilson Siringoringo <Wilson.Siringoringo@toiohomai.ac.nz>
2021-11-29 18:06:56 +1300

    Enabling HTTP GET request

--- WebServiceAsmx.Web/DayToDate.asmx.cs
+++ WebServiceAsmx.Web/DayToDate.asmx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
 using System.Web.Services;
 using WebServiceAsmx.Web.Classes;
 
@@ -20,6 +21,7 @@ namespace WebServiceAsmx.Web
         private static NextDayDateFinder _dateFinder = new NextDayDateFinder();
 
         [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Xml)]
         public string DayNextDate(string dayname)
         {
             DateTime nextDate = _dateFinder.GetNextDate(dayname);
